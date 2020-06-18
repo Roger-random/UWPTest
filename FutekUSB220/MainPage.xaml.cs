a24315b5ab0a5267f6d7464dc82fb3aafdacbea8
Roger Cheng <roger.random@outlook.com>
2020-06-18 01:48:35 -0700

    Read a line to see if it is expected format

--- FutekUSB220/MainPage.xaml.cs
+++ FutekUSB220/MainPage.xaml.cs
@@ -4,9 +4,15 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Foundation.Diagnostics;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -39,6 +45,8 @@ namespace FutekUSB220
             {
                 logger = ((App)Application.Current).logger;
             }
+
+            _ = Dispatcher.RunAsync(CoreDispatcherPriority.Low, EnumerateSerialDevices);
         }
         private void ActivityUpdateTimer_Tick(object sender, object e)
         {
@@ -55,6 +63,60 @@ namespace FutekUSB220
             else
             {
                 Debug.WriteLine("WARNING: Logger not available, log message lost.");
+            }
+        }
+
+        private async void EnumerateSerialDevices()
+        {
+            DeviceInformationCollection deviceinfos = await DeviceInformation.FindAllAsync(SerialDevice.GetDeviceSelector());
+
+            foreach (DeviceInformation deviceinfo in deviceinfos)
+            {
+                Log($"Serial device ID={deviceinfo.Id}");
+                try
+                {
+                    using (SerialDevice device = await SerialDevice.FromIdAsync(deviceinfo.Id))
+                    {
+                        if (device != null)
+                        {
+                            device.BaudRate = 9600;
+                            device.DataBits = 8;
+                            device.Parity = SerialParity.None;
+                            device.StopBits = SerialStopBitCount.One;
+                            device.ReadTimeout = new TimeSpan(0, 0, 1);
+                            device.WriteTimeout = new TimeSpan(0, 0, 2);
+
+                            using (DataReader reader = new DataReader(device.InputStream))
+                            {
+                                CancellationTokenSource cancelSrc = new CancellationTokenSource(2000); // Cancel after 2000 milliseconds
+
+                                reader.UnicodeEncoding = UnicodeEncoding.Utf8;
+                                reader.InputStreamOptions = InputStreamOptions.ReadAhead;
+
+                                Log("Small serial read expecting repeated readings");
+                                uint loadedSize = await reader.LoadAsync(18).AsTask<uint>(cancelSrc.Token);
+                                Log($"reader.LoadAsync returned {loadedSize} bytes");
+                                if (loadedSize == 18)
+                                {
+                                    string helloText = reader.ReadString(loadedSize);
+                                    Log($"Hello text = {helloText}");
+                                    // Verify expected format for load cell reading
+                                    if ((helloText.StartsWith('+') || helloText.StartsWith('-')) &&
+                                         helloText.EndsWith("lbs\r\n"))
+                                    {
+                                        double value = Double.Parse(helloText.Substring(0, 13));
+                                        Log($"Parsed value={value}");
+                                        // If we get this far, looks like a load cell to us!
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log(e.ToString(), LoggingLevel.Error);
+                }
             }
         }
     }
