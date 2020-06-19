af29ef784d77739f3c769687363aad29c1a24abd
Roger Cheng <roger.random@outlook.com>
2020-06-19 00:53:40 -0700

    Continuous loop reading sensor data

--- FutekUSB220/MainPage.xaml.cs
+++ FutekUSB220/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
@@ -11,6 +12,7 @@ using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Foundation.Diagnostics;
+using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -30,15 +32,26 @@ namespace FutekUSB220
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const string FutekUSB220DeviceId = "FutekUSB220";
+
         private DispatcherTimer activityUpdateTimer;
         private Logger logger = null;
+
+        private TimeSpan _sensorReportInterval = new TimeSpan(0, 0, 5);
+        private DateTime _previousSensorReport = DateTime.MinValue;
+        private UInt64 _sensorUpdatesSinceLastReport = 0;
+
+        private SerialDevice _serialDevice = null;
+        private DataReader _dataReader = null;
+
+        public double SensorValue { get; private set; } = 0;
 
         public MainPage()
         {
             this.InitializeComponent();
             activityUpdateTimer = new DispatcherTimer();
             activityUpdateTimer.Tick += ActivityUpdateTimer_Tick;
-            activityUpdateTimer.Interval = new TimeSpan(0, 0, 0, 0, 250 /* milliseconds */);
+            activityUpdateTimer.Interval = new TimeSpan(0, 0, 0, 0, 200 /* milliseconds */);
             activityUpdateTimer.Start();
 
             if (Application.Current as App != null)
@@ -46,24 +59,182 @@ namespace FutekUSB220
                 logger = ((App)Application.Current).logger;
             }
 
+            Application.Current.Suspending += Application_Suspending;
+            Application.Current.Resuming += Application_Resuming;
+
             _ = Dispatcher.RunAsync(CoreDispatcherPriority.Low, EnumerateSerialDevices);
         }
+
+        private async void Application_Resuming(object sender, object e)
+        {
+            await Connect((string)ApplicationData.Current.LocalSettings.Values[FutekUSB220DeviceId]);
+        }
+
+        private void Application_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            Disconnect();
+        }
+
         private void ActivityUpdateTimer_Tick(object sender, object e)
         {
             tbLogging.Text = logger.Recent;
             tbClock.Text = DateTime.UtcNow.ToString("yyyyMMddHHmmssff");
+            tbSensorValue.Text = $"{SensorValue} lbs";
         }
 
-        private void Log(string t, LoggingLevel level = LoggingLevel.Verbose)
+        private async void Log(string t, LoggingLevel level = LoggingLevel.Verbose)
         {
             if (logger != null)
             {
                 logger.Log(t, level);
+                if (level > LoggingLevel.Information)
+                {
+                    await logger.WriteLogBlock();
+                }
             }
             else
             {
                 Debug.WriteLine("WARNING: Logger not available, log message lost.");
             }
+        }
+
+        private async Task<double> ReadSensorReport()
+        {
+            double value = 0;
+
+            if (_serialDevice == null)
+            {
+                InvalidOperation("ReadSensorReport called before connecting serial device.");
+            }
+            if (_dataReader == null)
+            {
+                InvalidOperation("ReadSensorReport called before connecting data reader.");
+            }
+
+            CancellationTokenSource cancelSrc = new CancellationTokenSource(2000); // Cancel after 2000 milliseconds
+
+            uint loadedSize = await _dataReader.LoadAsync(18).AsTask<uint>(cancelSrc.Token);
+            if (loadedSize == 18)
+            {
+                string reportLine = _dataReader.ReadString(18);
+
+                // Match input string against expected pattern using regular expression
+                // C# Regular Expression syntax https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+                //
+                // ^ = must start with this
+                //   [+-] = must have one of either '+' or '-'
+                //     ^[+-] = must start with either '+' or '-'
+                // \d = numerical digit
+                //   + = one or more matches
+                //     \d+ = at least one digit
+                // . = one and only one decimal point
+                // (repeat "at least one digit" above)
+                // \s = space & similar spacing characters
+                //   + = one or more matches
+                //     \s+ = at least one spacing character
+                // lbs\r\n = literal string "lbs" followed by carriage return then line feed.
+                //   $ = end of the string
+                if (reportLine.Length == 18 && Regex.IsMatch(reportLine, "^[+-]\\d+.\\d+\\s+lbs\r\n$"))
+                {
+                    value = Double.Parse(reportLine.Substring(0, 13));
+                }
+                else
+                {
+                    IOError($"Expected length 18 actual {reportLine.Length}. String={reportLine}");
+                }
+            }
+            else
+            {
+                IOError($"DataReader read {loadedSize} bytes instead of the requested 18.");
+            }
+
+            return value;
+        }
+        private async void UpdateSensorValue()
+        {
+            try
+            {
+                SensorValue = await ReadSensorReport();
+                if (DateTime.UtcNow - _previousSensorReport > _sensorReportInterval)
+                {
+                    Log($"Sensor updated {_sensorUpdatesSinceLastReport} times in the past {_sensorReportInterval.TotalSeconds} seconds.");
+                    _previousSensorReport = DateTime.UtcNow;
+                    _sensorUpdatesSinceLastReport = 0;
+                }
+                else
+                {
+                    _sensorUpdatesSinceLastReport++;
+                }
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Low, UpdateSensorValue);
+            }
+            catch (Exception e)
+            {
+                Log($"UpdateSensorValue will not requeue itself due to {e.ToString()}", LoggingLevel.Error);
+            }
+        }
+
+
+        private void IOError(string message)
+        {
+            Log(message, LoggingLevel.Error);
+            throw new IOException(message);
+        }
+
+        private void InvalidOperation(string message)
+        {
+            Log(message, LoggingLevel.Error);
+            throw new InvalidOperationException(message);
+        }
+
+        private bool Connected()
+        {
+            return _serialDevice != null && _dataReader != null;
+        }
+
+        private async Task<bool> Connect(string deviceId)
+        {
+            try
+            {
+                Log($"Check if Futek USB220 is at {deviceId}");
+                _serialDevice = await SerialDevice.FromIdAsync(deviceId);
+                if (_serialDevice != null)
+                {
+                    _serialDevice.BaudRate = 9600;
+                    _serialDevice.DataBits = 8;
+                    _serialDevice.Parity = SerialParity.None;
+                    _serialDevice.StopBits = SerialStopBitCount.One;
+                    _serialDevice.ReadTimeout = new TimeSpan(0, 0, 1);
+                    _serialDevice.WriteTimeout = new TimeSpan(0, 0, 2);
+
+                    _dataReader = new DataReader(_serialDevice.InputStream);
+                    _dataReader.UnicodeEncoding = UnicodeEncoding.Utf8;
+
+                    double initialValue = await ReadSensorReport();
+                    Log($"Successfully read an initial value of {initialValue} lbs.");
+
+                    ApplicationData.Current.LocalSettings.Values[FutekUSB220DeviceId] = deviceId;
+
+                    return true;
+                }
+                Log($"Failed to acquire SerialDevice from {deviceId}");
+            }
+            catch (Exception e)
+            {
+                Log($"Probably not a Futek USB220 at {deviceId}");
+                Log($"Due to error encountered: {e.ToString()}");
+                Disconnect();
+            }
+
+            return false;
+        }
+
+        private void Disconnect()
+        {
+            Log("Disconnect Futek USB220");
+            _dataReader?.Dispose();
+            _dataReader = null;
+            _serialDevice?.Dispose();
+            _serialDevice = null;
         }
 
         private async void EnumerateSerialDevices()
@@ -73,49 +244,10 @@ namespace FutekUSB220
             foreach (DeviceInformation deviceinfo in deviceinfos)
             {
                 Log($"Serial device ID={deviceinfo.Id}");
-                try
+                if (await Connect(deviceinfo.Id))
                 {
-                    using (SerialDevice device = await SerialDevice.FromIdAsync(deviceinfo.Id))
-                    {
-                        if (device != null)
-                        {
-                            device.BaudRate = 9600;
-                            device.DataBits = 8;
-                            device.Parity = SerialParity.None;
-                            device.StopBits = SerialStopBitCount.One;
-                            device.ReadTimeout = new TimeSpan(0, 0, 1);
-                            device.WriteTimeout = new TimeSpan(0, 0, 2);
-
-                            using (DataReader reader = new DataReader(device.InputStream))
-                            {
-                                CancellationTokenSource cancelSrc = new CancellationTokenSource(2000); // Cancel after 2000 milliseconds
-
-                                reader.UnicodeEncoding = UnicodeEncoding.Utf8;
-                                reader.InputStreamOptions = InputStreamOptions.ReadAhead;
-
-                                Log("Small serial read expecting repeated readings");
-                                uint loadedSize = await reader.LoadAsync(18).AsTask<uint>(cancelSrc.Token);
-                                Log($"reader.LoadAsync returned {loadedSize} bytes");
-                                if (loadedSize == 18)
-                                {
-                                    string helloText = reader.ReadString(loadedSize);
-                                    Log($"Hello text = {helloText}");
-                                    // Verify expected format for load cell reading
-                                    if ((helloText.StartsWith('+') || helloText.StartsWith('-')) &&
-                                         helloText.EndsWith("lbs\r\n"))
-                                    {
-                                        double value = Double.Parse(helloText.Substring(0, 13));
-                                        Log($"Parsed value={value}");
-                                        // If we get this far, looks like a load cell to us!
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Log(e.ToString(), LoggingLevel.Error);
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Low, UpdateSensorValue);
+                    break;
                 }
             }
         }
