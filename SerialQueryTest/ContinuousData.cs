3272f360cc3cb84c8626a22b9ccfa6669190741b
Roger Cheng <roger.random@outlook.com>
2020-09-22 20:39:09 -0700

    Fewer magic numbers, more constants.

--- SerialQueryTest/ContinuousData.cs
+++ SerialQueryTest/ContinuousData.cs
@@ -17,6 +17,11 @@ namespace SerialQueryTest
     class ContinuousData
     {
         private const string LABEL = "continuous data device";
+        private const int TASK_CANCEL_TIMEOUT = 150; // milliseconds
+        // Expected format: "+0.00000     lbs\r\n"
+        private const int EXPECTED_LENGTH = 18;
+        private const int DATA_LENGTH = 13; // When parsing as double, stop just before this character index.
+        private const string DELIMITER = "\r\n";
 
         private SerialDevice _serialDevice = null;
         private Logger _logger = null;
@@ -29,7 +34,7 @@ namespace SerialQueryTest
 
         public async Task<bool> IsDeviceOnPort(string deviceId)
         {
-            String sampleData = null;
+            double sampleData = 0.0;
 
             _logger.Log($"Checking if this is a {LABEL}: {deviceId}", LoggingLevel.Information);
             try
@@ -41,41 +46,36 @@ namespace SerialQueryTest
                     _serialDevice.DataBits = 8;
                     _serialDevice.Parity = SerialParity.None;
                     _serialDevice.StopBits = SerialStopBitCount.One;
-                    _serialDevice.ReadTimeout = new TimeSpan(0, 0, 1);
+                    _serialDevice.ReadTimeout = new TimeSpan(0, 0, 0, 0, TASK_CANCEL_TIMEOUT /* milliseconds */);
 
                     _dataReader = new DataReader(_serialDevice.InputStream);
                     _dataReader.UnicodeEncoding = UnicodeEncoding.Utf8;
 
                     try
                     {
-                        /* Test resync with offset data
-                        try
+                        sampleData = await nextData(2000); // 2 second timeout for the first read.
+                        _logger.Log($"Successfully retrieved {sampleData} from {LABEL} on {deviceId}");
+
+                        // Next five should be fast.
+                        for (int i = 0; i < 5; i++)
                         {
-                            CancellationTokenSource cancelSrc = new CancellationTokenSource(2000); // Cancel after 2000 milliseconds
-                            await _dataReader.LoadAsync(1).AsTask<uint>(cancelSrc.Token);
-                            _ = _dataReader.ReadString(1);
+                            _logger.Log($"Follow-up data {await nextData()}");
                         }
-                        catch(TaskCanceledException)
-                        {
-                        }
-                        */ 
-                        sampleData = await nextData();
-                        if (sampleData == null)
-                        {
-                            _logger.Log($"No string so inferring {LABEL} is not at {deviceId}");
-                            return false;
-                        }
-                        else
-                        {
-                            _logger.Log($"Look slike {LABEL} is at {deviceId}");
-                            return true;
-                        }
+                        return true;
                     }
                     catch (TaskCanceledException)
                     {
                         _logger.Log($"Timed out so inferring {LABEL} is not at {deviceId}");
-                        return false;
                     }
+                    catch (IOException)
+                    {
+                        _logger.Log($"Encountered error indicating {LABEL} is not at {deviceId}");
+                    }
+                }
+                else
+                {
+                    // This is a high priority error because we expect be able to open the port to take look.
+                    _logger.Log($"Unable to get SerialDevice for {LABEL} on {deviceId}", LoggingLevel.Error);
                 }
             }
             catch (Exception e)
@@ -92,23 +92,45 @@ namespace SerialQueryTest
             return false;
         }
 
-        private async Task<string> nextData()
+        private bool validFormat(string inputData)
+        {
+            // Match input string against expected pattern using regular expression
+            // C# Regular Expression syntax https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+            //
+            // ^ = must start with this
+            //   [+-] = must have one of either '+' or '-'
+            //     ^[+-] = must start with either '+' or '-'
+            // \d = numerical digit
+            //   + = one or more matches
+            //     \d+ = at least one digit
+            // . = one and only one decimal point
+            // (repeat "at least one digit" above)
+            // \s = space & similar spacing characters
+            //   + = one or more matches
+            //     \s+ = at least one spacing character
+            // lbs\r\n = literal string "lbs" followed by carriage return then line feed.
+            //   $ = end of the string
+            return Regex.IsMatch(inputData, "^[+-]\\d+.\\d+\\s+lbs\r\n$");
+        }
+
+        private async Task<double> nextData(int cancelTimeout = TASK_CANCEL_TIMEOUT)
         {
             string inString = null;
+            double parsedValue = 0.0;
             int deliminiterIndex = 0;
             int syncRemainder = 0;
 
             if (_serialDevice == null)
             {
-                InvalidOperation("ReadSensorReport called before connecting serial device.");
+                InvalidOperation("nextData called before connecting serial device.");
             }
             if (_dataReader == null)
             {
-                InvalidOperation("ReadSensorReport called before connecting data reader.");
+                InvalidOperation("nextData called before connecting data reader.");
             }
 
-            CancellationTokenSource cancelSrc = new CancellationTokenSource(2000); // Cancel after 2000 milliseconds
-            uint loadedSize = await _dataReader.LoadAsync(18).AsTask<uint>(cancelSrc.Token);
+            CancellationTokenSource cancelSrc = new CancellationTokenSource(cancelTimeout);
+            uint loadedSize = await _dataReader.LoadAsync(EXPECTED_LENGTH).AsTask<uint>(cancelSrc.Token);
             if (loadedSize > 0)
             {
                 try
@@ -118,83 +140,71 @@ namespace SerialQueryTest
                 catch (ArgumentOutOfRangeException)
                 {
                     // This is thrown when the data can't be parsed as UTF8, interpreting to mean the incoming
-                    // data was not set at the baud rate of the expected device.
-                    _logger.Log($"Non UTF-8 data encountered. This is only expected when probing ports for {LABEL}");
-                    return null;
+                    // data was not sent at the baud rate of the expected device.
+                    IOError($"Non UTF-8 data encountered. This is expected when {LABEL} is not on this port.");
                 }
 
-                deliminiterIndex = inString.IndexOf("\r\n");
-                if (deliminiterIndex != 16)
+                deliminiterIndex = inString.IndexOf(DELIMITER);
+                if (deliminiterIndex != (EXPECTED_LENGTH-DELIMITER.Length))
                 {
-                    // Oh no, the end deliminiter is not where we expected it to be. Can we recover?
-                    if (inString.Length < 18)
+                    // Resync Mode: the end deliminiter is not where we expected it to be. Can we recover?
+                    if (inString.Length < EXPECTED_LENGTH)
                     {
-                        // Sometimes we just don't get all 18 we asked for and just need to ask for the rest.
-                        syncRemainder = 18 - inString.Length;
+                        // Occasionally not all 18 requested bytes came in, and we just need to ask for the rest.
+                        syncRemainder = EXPECTED_LENGTH - inString.Length;
                     }
                     else if (deliminiterIndex == -1)
                     {
                         // If we have the full length yet there's no delimiter at all in the string we retrieved,
                         // then the problem is worse than just getting offset data.
-                        InvalidOperation($"No deliminter in data retrieved from {LABEL}");
+                        IOError($"No deliminter in data retrieved from {LABEL}");
                     }
                     else
                     {
                         // Delimiter is somewhere in the middle instead of the end. Discard the end of the
                         // truncated prior segment, keep the start of the latter segment, and try to read data
                         // to complete the latter segment.
-                        inString = inString.Substring(deliminiterIndex + 2);
-                        syncRemainder = 18 - inString.Length;
+                        inString = inString.Substring(deliminiterIndex + DELIMITER.Length);
+                        syncRemainder = EXPECTED_LENGTH - inString.Length;
                     }
 
                     if (syncRemainder < 0)
                     {
-                        InvalidOperation($"Expected inString.Length to be 18 or less, but is {inString.Length}");
+                        // This means a dumb arithmetic error was made earlier.
+                        IOError($"Expected inString.Length to be {EXPECTED_LENGTH} or less, but is {inString.Length}");
                     }
 
-                    cancelSrc = new CancellationTokenSource(1000); // Cancel after 1 second
+                    cancelSrc = new CancellationTokenSource(cancelTimeout);
                     loadedSize = await _dataReader.LoadAsync((uint)syncRemainder).AsTask<uint>(cancelSrc.Token);
-
                     if (loadedSize != syncRemainder)
                     {
-                        InvalidOperation($"Failed to resync, want {syncRemainder} but got {loadedSize}");
+                        // If we are really just picking up a few straggler bytes, this would be easy.
+                        // If it is a problem, something else is wrong.
+                        IOError($"Failed to resync, want {syncRemainder} but got {loadedSize}");
                     }
+
+                    // Not checking for ArgumentOutOfRangeException here because if baud rate is wrong we should
+                    // have already triggered ArgumentOutOfRangeException in the earlier ReadString().
                     inString += _dataReader.ReadString(loadedSize);
 
-                    if (16 != inString.IndexOf("\r\n"))
+                    if (16 != inString.IndexOf(DELIMITER))
                     {
-                        InvalidOperation($"Failed to resync, only got {inString.Length} long string: {inString}");
+                        IOError($"Failed to resync, only got {inString.Length} long string: {inString}");
                     }
                 }
 
-                // Match input string against expected pattern using regular expression
-                // C# Regular Expression syntax https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
-                //
-                // ^ = must start with this
-                //   [+-] = must have one of either '+' or '-'
-                //     ^[+-] = must start with either '+' or '-'
-                // \d = numerical digit
-                //   + = one or more matches
-                //     \d+ = at least one digit
-                // . = one and only one decimal point
-                // (repeat "at least one digit" above)
-                // \s = space & similar spacing characters
-                //   + = one or more matches
-                //     \s+ = at least one spacing character
-                // lbs\r\n = literal string "lbs" followed by carriage return then line feed.
-                //   $ = end of the string
-                if (!Regex.IsMatch(inString, "^[+-]\\d+.\\d+\\s+lbs\r\n$"))
+                if (!validFormat(inString))
                 {
-                    InvalidOperation($"Improper format string {inString}");
+                    IOError($"Improper format string {inString}");
                 }
-                //value = Double.Parse(reportLine.Substring(0, 13));
-                return inString;
+                parsedValue =  Double.Parse(inString.Substring(0, DATA_LENGTH));
             }
             else
             {
-                _logger.Log("Unexpected: LoadAsync() returned with zero bytes.", LoggingLevel.Critical);
-                return null;
+                IOError("Unexpected: LoadAsync() returned with zero bytes.");
             }
+
+            return parsedValue;
         }
 
         private void IOError(string message)
